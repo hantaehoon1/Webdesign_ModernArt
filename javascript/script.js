9121ba2d5fe2e1c467da2ca222100c77bd526457
hantaehoon1 <hantaehoon1@gmail.com>
2022-11-08 13:47:28 +0900

    2022-11-8 작성

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,30 @@
 jQuery(document).ready(function(){
-    $('.navi>li').mouseover(function(){
-        $(this).find('.submenu').stop().slideDown(500);
+    $('.navi').mouseover(function(){
+      $(this).find('.submenu').stop().slideDown(500)
+        $('#menuwrap').stop().animate({'height':'250px'});
     }).mouseout(function(){
-        $(this).find('.submenu').stop().slideUp(500);
+      $(this).find('.submenu').stop().slideUp(500)
+        $('#menuwrap').stop().animate({'height':'100px'});
     });
 
-    setInterval(function () {
-      $(".slidelist").delay(1000);
-      $(".slidelist").animate({ marginTop: -300 });
-      $(".slidelist").delay(2000);
-      $(".slidelist").animate({ marginTop: -600 });
-      $(".slidelist").delay(2000);
-      $(".slidelist").animate({ marginTop: 0 });
-      $(".slidelist").delay(2000);
-    });
+    // setInterval(function () {
+    //   $(".slidelist").delay(1000);
+    //   $(".slidelist").animate({ marginTop: -300 });
+    //   $(".slidelist").delay(2000);
+    //   $(".slidelist").animate({ marginTop: -600 });
+    //   $(".slidelist").delay(2000);
+    //   $(".slidelist").animate({ marginTop: 0 });
+    //   $(".slidelist").delay(2000);
+    // });
+
+    $('.imgslide a:gt(0)').hide();
+    setInterval(function(){
+      $('.imgslide a:first-child').fadeOut()
+      .next('a')
+      .fadeIn()
+      .end()
+      .appendTo('.imgslide');
+    }, 3000);
     
     $(function(){
       $('.tabmenu>li>a').click(function(){
@@ -24,16 +35,14 @@ jQuery(document).ready(function(){
       });
     });
 
-    $(function(){
+    
       $('.notice li:first').click(function(){
         $('#modal').addClass('active');
       });
-    });
     
-    $(function(){
       $('.btn').click(function(){
-        $('#modal').removeClass('active')
+        $('#modal').removeClass('active');
       });
-    });
 
 });
+
